0d2fb99ba480afcdff9100b5c106935a5c735779
raptor8134 <jamesnotley@gmail.com>
2021-06-01 22:33:20 -0700

    update linkconfig.sh and the directories to not be stupid

--- linkconfig.sh
+++ linkconfig.sh
@@ -1,17 +1,16 @@
-homedirfile="$(ls -A | egrep '^\.' | egrep -v '(^.git$|^.gitignore$)')"
+homedirfile="$(ls -A home/)"
 echo $homedirfile
 for x in $homedirfile; do 
 	rm -rf $HOME/$x
 	echo Removed $HOME/$x
-	ln $(pwd)/$x $HOME/$x
+	ln $(pwd)/home/$x $HOME/$x
 	echo Linked $x to $HOME/$x
 done
-dotconfigs="$(ls -d */ | tr -d '/')"
-dotconfigs="$dotconfigs $(ls *.conf *.yaml *rc)"
-for x in $dotconfigs; do
+xdgconfigs="$(ls config/)"
+for x in $xdgconfigs; do
 	rm -rf $XDG_CONFIG_HOME/$x
 	echo Removed $XDG_CONFIG_HOME$x
-	ln -s $(pwd)/$x ~/.config/$x
+	ln -s $(pwd)/config/$x ~/.config/$x
 	echo Linked $XDG_CONFIG_HOME$x to ./$x
 done
 git add .
